879dee1c5068b7b3fe90f7644aa2fa26fd228448
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2026-02-25 15:36:51 +0530

    Update for ECR pipeline check

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Docker and Swanand! 🚀'
+    return 'Hello from Docker and Swanand S. Joshi! 🚀'
 
 @app.route('/health')
 def health():
